8f364f953067071753ce4ec005e1e8035369faff
Dana Sharon <whosedreamisthis@gmail.com>
2025-05-27 13:13:56 -0700

    bug fix ball was not taking paddle   y location

--- ball.py
+++ ball.py
@@ -3,16 +3,16 @@ from consts import *
 
 class Ball:
     def __init__(self):
-        self.x = SCREEN_WIDTH // 2
-        self.y = SCREEN_HEIGHT // 2 
         self.speed = 2
         self.dx = random.choice([-1, 1]) # Initial horizontal direction (-1 for left, 1 for right)
         self.dy = random.choice([-1, 1])
         self.radius = 10
+        self.x = SCREEN_WIDTH // 2 + self.radius
+        self.y = SCREEN_HEIGHT // 2 + self.radius
 
     def reset(self):
-        self.x = SCREEN_WIDTH // 2
-        self.y = SCREEN_HEIGHT // 2 
+        self.x = SCREEN_WIDTH // 2 + self.radius
+        self.y = SCREEN_HEIGHT // 2 + self.radius
         self.dx = random.choice([-1, 1]) # Initial horizontal direction (-1 for left, 1 for right)
         self.dy = random.choice([-1, 1])
                 
--- main.py
+++ main.py
@@ -9,10 +9,12 @@ pygame.display.set_caption("Pong")
 clock = pygame.time.Clock()
 FPS = 60
 running = True
+paused = True
 score_manager = ScoreManager()
 player = Paddle (SCREEN_WIDTH - 30,SCREEN_HEIGHT//2)
 enemy = Paddle (30,SCREEN_HEIGHT//2)
 ball = Ball()
+
 def draw_line(screen):
     segment_length = 30
     gap_length = 20
@@ -35,27 +37,48 @@ def move_enemy():
     #     enemy.move_up()
 
 def check_out_of_bounds():
+    global paused
     if ball.x < -ball.radius:
         score_manager.increase_player_score()
         ball.reset()
+        paused = True
     elif ball.x > SCREEN_WIDTH:
         score_manager.increase_enemy_score()
         ball.reset()
+        paused = True
+
+def check_collision():
+    if ball.x >= player.x - ball.radius\
+        and player.y - player.height/2 <= ball.y <= player.y + player.height/2:
+        ball.dx *= -1
+        print(f"1 check collision: {ball.x} {ball.y} {ball.dx} {ball.dy}")
+    elif ball.x < enemy.x + ball.radius + player.width /2\
+        and enemy.y - enemy.height/2 <= ball.y <= enemy.y + enemy.height/2:
+        ball.dx *= -1
+        print(f"1 check collision: {ball.x} {ball.y} {ball.dx} {ball.dy}")
+
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                paused = not paused
             
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP]:
-        player.move_up()
-    elif keys[pygame.K_DOWN]:
-        player.move_down()
-    
-    ball.update()
-    check_out_of_bounds()
-    move_enemy()
+    if not paused:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            player.move_up()
+            enemy.move_up()
+        elif keys[pygame.K_DOWN]:
+            player.move_down()
+            enemy.move_down()
+        
+        ball.update()
+        check_out_of_bounds()
+        check_collision()
+        move_enemy()
     screen.fill(BLACK)
     draw_line(screen)
     score_manager.draw(screen)
